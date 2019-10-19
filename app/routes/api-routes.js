28c72461b108a3fd5f5fa32bf925ef1a0c500fc8
shijir0927 <shijiree_mng@yahoo.com>
2019-10-19 13:05:23 -0700

    presentation changes

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -57,37 +57,16 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/answer.html"));
   });
 
+  app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/contact.html"));
+  });
+
+  app.get("/signup", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
   // Displays a single character, or returns false
-  app.get("/api/all/:character", function(req, res) {
-    var chosen = req.params.character;
-
-    console.log(chosen);
-
-    for (var i = 0; i < characters.length; i++) {
-      if (chosen === characters[i].routeName) {
-        return res.json(characters[i]);
-      }
-    }
-
-    return res.json(false);
-  });
-
-  // Create New Characters - takes in JSON input
-  app.post("/api/characters", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newCharacter = req.body;
-
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newCharacter.routeName = newCharacter.name
-      .replace(/\s+/g, "")
-      .toLowerCase();
-
-    console.log(newCharacter);
-
-    characters.push(newCharacter);
-
-    res.json(newCharacter);
-  });
 };
